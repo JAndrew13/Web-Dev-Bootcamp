85e63f306323f9d0db0fd16e033ca20127ee785e
Jake Brunner <Jbbrunner10@gmail.com>
2022-07-13 16:42:17 -0700

    create simple web server and web calculator

--- MyExpressServer/Server.js
+++ MyExpressServer/Server.js
@@ -10,6 +10,14 @@ app.get("/", function(req, res){
     res.send("hello world");
 });
 
+app.get("/contact", function(req, res){
+    res.send("Call me up dude!");
+});
+
+app.get("/about", function(req, res){
+    res.send("Im just a guy trying to code my way up.");
+});
+
 // Server listen alert on startup
 app.listen(3000, function(){
   console.log("Server started on Port : 3000");
