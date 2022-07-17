5885f4ed0dcdcae40f5148d91afad29925b66424
Jake Brunner <Jbbrunner10@gmail.com>
2022-07-17 10:14:20 -0700

    add functionality to To-Do list app

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,22 +1,50 @@
 //jshint esversion:6
 
+// required packages
 const express = require("express");
 const bodyParser = require("body-parser");
 
+// set app to express
 const app = express();
 
-app.get("/", function(res, req){
+//  create an array of list items
+let items = ["Buy Food", "Cook Food", "Eat Food"];
 
-  var today = new Data();
-  var currentDay = today.getDay();
+// set EJS view engine
+app.set("view engine", "ejs");
 
-  if (currentDay === 6 || currentDay === 0) {
-    res.sendFile(__dirname + "weekend.html");
-  } else {
-    res.sendFile(__dirname + "weekday.html")
-  }
+// connect body-parser, public folder
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+// get/set todays date and connect to EJS template
+app.get("/", function(req, res){
+
+  //get todays date
+  let today = new Date();
+
+  //set options for text display
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  // set day variable to processed date string
+  let day = today.toLocaleDateString("en-US", options);
+
+  // use EJS to render the text on Clients browser
+  res.render("list", {kindOfDay: day, newListItems: items});
 });
 
-app.listen(3000, function(){
+// handle post requests to home route
+app.post("/", function(req, res){
+  let item = req.body.newItem; // sets value of entered text to new item.
+  items.push(item);            // adds new item to list of Items
+  res.redirect("/");           // redirects client to home with new data
+})
+
+// listen for server startup and display message when running
+app.listen(3000, function() {
   console.log("server is up on port 3000.");
 });
