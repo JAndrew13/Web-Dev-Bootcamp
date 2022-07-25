45991c8dcb6d2c4ea4dcad5f025c285d52d51a1d
Jake Brunner <Jbbrunner10@gmail.com>
2022-07-25 08:04:49 -0700

    Level 2 - DB Encryption, .env variables, and .gitignore

--- SecretsApp/app.js
+++ SecretsApp/app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const express = require("express");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -13,12 +14,20 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+
+//connect App to Database
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
-const userSchema = {
+// Create user schema with encryption
+const userSchema = new mongoose.Schema ({
   email: String,
   password: String
-};
+});
+
+// encryption secret key & DB plugin
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+
 const User = new mongoose.model("User", userSchema);
 
 // ========================= //
@@ -38,7 +47,7 @@ app.get("/register", function(req, res){
   res.render("register");
 });
 
-//
+// Post @ Registration
 app.post("/register", function(req, res){
   const newUser = new User({
     email: req.body.username,
@@ -53,6 +62,7 @@ app.post("/register", function(req, res){
   });
 });
 
+// Post @ Login
 app.post("/login", function(req, res){
   const username = req.body.username;
   const password = req.body.password;
