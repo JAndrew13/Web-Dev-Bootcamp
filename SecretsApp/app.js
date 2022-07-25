5539ec53abd132a35c4e2f61194db66d6badd1e3
Jake Brunner <Jbbrunner10@gmail.com>
2022-07-25 08:29:44 -0700

    Level 3 - Add Password Hashing

--- SecretsApp/app.js
+++ SecretsApp/app.js
@@ -4,8 +4,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const express = require("express");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-
+const md5 = require("md5");
 
 const app = express();
 
@@ -25,9 +24,6 @@ const userSchema = new mongoose.Schema ({
   password: String
 });
 
-// encryption secret key & DB plugin
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -52,7 +48,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err){
     if (err) {
@@ -66,7 +62,7 @@ app.post("/register", function(req, res){
 // Post @ Login
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
   User.findOne({email: username}, function(err, foundUser){
     if (err){
       console.log(err);
