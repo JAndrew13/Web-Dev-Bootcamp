aedfc4c9ea0bb6af52d0aad52995fb1a9c3a3e61
Jake Brunner <Jbbrunner10@gmail.com>
2022-07-25 20:25:21 -0700

    Level 4 - Bcrypt Hash/Salt

--- SecretsApp/app.js
+++ SecretsApp/app.js
@@ -4,8 +4,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const express = require("express");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
 
+const saltRounds = 10;
 const app = express();
 
 app.set("view engine", "ejs");
@@ -46,35 +47,42 @@ app.get("/register", function(req, res){
 
 // Post @ Registration
 app.post("/register", function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err){
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
-  newUser.save(function(err){
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
-  });
+
+
 });
 
 // Post @ Login
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
   User.findOne({email: username}, function(err, foundUser){
     if (err){
       console.log(err);
     } else {
       if (foundUser){
-        if (foundUser.password === password) {
-          res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result){
+          if (result === true){
+            res.render("secrets");
+          }
+        });
       }
     }
   });
 });
+
 
 // ========================= //
 
