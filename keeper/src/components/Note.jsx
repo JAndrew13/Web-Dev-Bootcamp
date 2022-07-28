02af1e686160897ca4bc51fa3c74713b66572478
Jake Brunner <Jbbrunner10@gmail.com>
2022-07-28 08:33:46 -0700

    Create Initial Project Tree

---
+++ keeper/src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Note(){
+        return  <note>
+                    <div className="note">
+                        <h1>Note Title</h1>
+                        <p>Some note details</p>
+                    </div>
+                </note>
+};
+
+export default Note;
