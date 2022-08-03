4561dda8587e948340ba737e3a6fcb5ebf03727f
Jake Brunner <Jbbrunner10@gmail.com>
2022-08-03 10:40:09 -0700

    add note create/delete functions

--- keeper/src/components/Note.jsx
+++ keeper/src/components/Note.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-function Note(props){
-    return  <note>
-                <div className="note">
-                    <h1>{props.title}</h1>
-                    <p>{props.content}</p>
-                </div>
-            </note>
-};
+
+function Note(props) {
+  function handleClick() {
+    props.onDelete(props.id);
+  }
+
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick={handleClick}>DELETE</button>
+    </div>
+  );
+}
 
 export default Note;
